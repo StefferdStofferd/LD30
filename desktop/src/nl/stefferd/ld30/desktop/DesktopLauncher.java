beda1128f5ec26a87b9966574fcafc2a5c081d36
SteFilms <s.siekman@gmail.com>
2014-08-22 22:31:30 +0200

    Ludum Dare Project Setup
    The setup for the project

---
+++ desktop/src/nl/stefferd/ld30/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nl.stefferd.ld30.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.stefferd.ld30.LdThirty;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LdThirty(), config);
+	}
+}

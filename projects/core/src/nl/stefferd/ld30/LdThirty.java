b7a42b54764ea9d251ddcac11b1a1b927462b601
SteFilms <s.siekman@gmail.com>
2014-08-23 04:09:59 +0200

    Assets testing Tested the assets loader

--- projects/core/src/nl/stefferd/ld30/LdThirty.java
+++ projects/core/src/nl/stefferd/ld30/LdThirty.java
@@ -1,15 +1,22 @@
 package nl.stefferd.ld30;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LdThirty extends Game {
+	
+	SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		Assets.load();
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.draw(Assets.tileDemo, 0, 0);
+		batch.end();
 	}
 }
